3e288f635ae2f5f68558e10ae5aacff11bbed44b
Aniruth Sivakumar <aniruth@email.sc.edu>
2022-04-27 10:18:36 -0400

    App Updates

--- app/src/main/java/com/example/assignment5/ContactDetailsActivity.kt
+++ app/src/main/java/com/example/assignment5/ContactDetailsActivity.kt
@@ -1,29 +1,30 @@
-
 package com.example.assignment5
-
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class ContactDetails : AppCompatActivity() {
+
+class ContactDetailsActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val contactHeading : TextView = findViewById(R.id.header)
-        val contactDescription : TextView = findViewById(R.id.description)
-        val contactImage : ImageView = findViewById(R.id.title)
-        val bundle :Bundle?= intent.extras
+        val contactHeading: TextView = findViewById(R.id.header)
+        val contactDescription: TextView = findViewById(R.id.description)
+        val contactImage: ImageView = findViewById(R.id.title)
+        val bundle: Bundle? = intent.extras
 
         val header = bundle!!.getString("heading")
-        val imageId= bundle!!.getInt("imageId")
+        val imageId = bundle!!.getInt("imageId")
         val description = bundle!!.getString("description")
 
         contactHeading.text = header
         contactDescription.text = description
         contactImage.setImageResource(imageId)
+    }
 
-    }}
+}
